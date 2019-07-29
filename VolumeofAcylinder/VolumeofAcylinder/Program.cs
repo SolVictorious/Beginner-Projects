c1e7fc6975ad5e18426677ab3722bd41ad29e5ef
Fahad Alghamdi <fahad86alghamdi@gmail.com>
2019-07-29 10:12:16 -0700

    Add files via upload

---
+++ VolumeofAcylinder/VolumeofAcylinder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VolumeofAcylinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159265;
+            int radius= 5;
+            int height= 7;
+            double volume = pi * (radius * radius) * height;
+            Console.WriteLine("The Volume of this Cylinder is {0}",volume);
+            Console.Read();
+
+        }
+    }
+}
