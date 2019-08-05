f7ad2eabe54bf7e85f8ce888e914017a41c44862
Fahad Alghamdi <fahad86alghamdi@gmail.com>
2019-08-05 11:04:14 -0700

    Add files via upload

---
+++ ObjectoOriantedProgramming/ObjectoOriantedProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectoOriantedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            jobs wizard = new jobs("Wizard",8,12,7,5);
+            wizard.Description();
+
+            jobs warrior = new jobs("Warrior", 12, 5, 7, 10);
+            warrior.Description();
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ ObjectoOriantedProgramming/ObjectoOriantedProgramming/jobs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectoOriantedProgramming
+{
+    class jobs
+    {
+        public string jobName;
+        int Str,Int,Wis,End,HP,MP;
+        public jobs(string jobname ,int str,int mind,int wis,int end)
+        {
+            this.jobName = jobname;
+            this.Str = str; this.Int = mind; this.Wis = wis; this.End = end;
+            this.HP = End * 10; this.MP = Int * 2;
+        }
+
+        public void Description()
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine(jobName);
+            Console.WriteLine("Health Points {0} , Mana Points {1} ",HP,MP);
+            Console.WriteLine("Strength is {0} ,\nIntelligence is {1}\nWisdom is {2}\nEndurance is {3}",Str,Int,Wis,End);
+            Console.WriteLine("=============================");
+
+        }
+
+        
+    }
+}
